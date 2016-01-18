df77b90dce5ba73343197dd09cc1ef727dc1e303
Xinyuan Zhang <xiz016@ucsd.edu>
2016-01-18 15:15:34 -0800

    javascript lab

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -39,10 +39,12 @@ function projectClick(e) {
 
     var containingProject = $(this).closest(".project"); 
     var description = $(containingProject).find(".project-description");
+   	var image = $(containingProject).find("img");
     if (description.length == 0) { 
        $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>"); 
     } else { 
-       description.fadeOut();
+       description.fadeToggle();
+       image.fadeToggle();
     }
 
 }
